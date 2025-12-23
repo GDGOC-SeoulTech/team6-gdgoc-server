95546cbec1a638ccc51e9a3dfb71a5bd1ac1c3e3
chaeyoungeee <chaeinge@gmail.com>
2025-12-23 16:55:30 +0900

    feat: cors 설정 추가

---
+++ src/main/java/com/gdgoc/arcive/global/config/CorsConfig.java
@@ -0,0 +1,35 @@
+package com.gdgoc.arcive.global.config;
+
+import java.util.List;
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+import org.springframework.http.HttpHeaders;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class CorsConfig implements WebMvcConfigurer {
+
+    private static  final String CLIENT = "";
+
+    public static CorsConfigurationSource apiConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+
+        configuration.setAllowedOrigins(List.of(
+                "http://localhost:3000",
+                "https://" + CLIENT
+        ));
+
+        configuration.setAllowedMethods(List.of("HEAD", "POST", "GET", "DELETE", "PUT", "OPTIONS", "PATCH"));
+        configuration.setAllowedHeaders(List.of("*"));
+        configuration.setAllowCredentials(true);
+        configuration.setExposedHeaders(List.of(HttpHeaders.LOCATION, HttpHeaders.SET_COOKIE));
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+
+        return source;
+    }
+}
